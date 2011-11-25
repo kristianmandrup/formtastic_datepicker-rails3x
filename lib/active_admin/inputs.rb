85e3d0fb1919e5322829a8946b7ec44a50f50137
Kristian Mandrup <kristian@unity3d.com>
2011-11-25 12:36:03 +0100

    initial

---
+++ lib/active_admin/inputs.rb
@@ -0,0 +1,10 @@
+module ActiveAdmin
+  module Inputs
+    extend ActiveSupport::Autoload
+
+    autoload :DatepickerInput
+    autoload :DateTimepickerInput
+  end
+end
+
+load 'active_admin/inputs/datepicker_input.rb'
---
+++ lib/formtastic_datepicker_inputs.rb
@@ -0,0 +1,45 @@
+# FormtasticDatepickerInputs
+module Formtastic::Inputs
+
+  class DatePickerInput < Formtastic::Inputs::StringInput
+
+    def to_html
+      input_wrapping do
+        label_html <<
+          builder.text_field(input_name, input_html_options)
+      end
+    end
+
+    def format
+      input_options[:format] || '%d %b %Y'
+    end
+
+    def value
+      input_options[:value] || object.send(method).try(:strftime, format)
+    end
+
+    def css_class
+      "ui-date-picker"
+    end
+
+    def input_html_options
+      new_class = [super[:class], css_class].compact.join(" ")
+      super.update(:class => new_class, :value => value)
+    end
+  end
+
+  class DateTimePickerInput < Formtastic::Inputs::DatePickerInput
+
+    def format
+      input_options[:format] || '%d %b %Y %H:%M'
+    end
+
+    def css_class
+      "ui-datetime-picker"
+    end
+
+  end
+
+end
+
+require 'active_admin/inputs' if defined?(ActiveAdmin)
